f8a571104db700f1dd3b0be021243458b4ff506c
Steve Chepp <stevechepp@gmail.com>
2016-02-17 11:21:21 -0600

    Initial Commit

---
+++ Intermediate/Build/Win64/UE4Editor/Development/UETutorial/PCH.UETutorial.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\My Stuff\Projects\Unreal Engine 4 Projects\PowerUpTutorial 4.9\Source\UETutorial\UETutorial.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Development/UETutorial/UETutorial.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UETUTORIAL_H__
+#define __AUTO_UETUTORIAL_H__
+//Last Write: 10/10/2015 6:31:04 PM +00:00
+#include "C:\My Stuff\My Projects\Unreal Engine 4\PowerUpTutorial\Source\UETutorial\UETutorial.h"
+#endif//__AUTO_UETUTORIAL_H__
